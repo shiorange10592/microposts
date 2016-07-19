70ee186048a39165fa050c6e3d709cad411c54c6
hideto shiozawa <shiorange10592@yahoo.co.jp>
2016-07-19 16:49:06 +0000

    Kadai first

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
   
   def create
     @user = User.new(user_params)
+    
     if @user.save
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
@@ -19,10 +20,37 @@ class UsersController < ApplicationController
     end
   end
   
+
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    
+    @user = User.find(params[:id])
+    puts '----------------------------'
+    puts @user.update(user_params)
+    puts '-------------------------------'
+    puts user_params
+    
+    if @user.update(user_params)
+      # 保存に成功し  た場合はトップページへリダイレクト
+        redirect_to user_path(current_user.id) , notice: 'ユーザ情報を更新しました'
+    else  
+        # 保存に失敗した場合は編集画面へ戻す
+        render 'edit'
+    end
+    
+    
+  end
+
+
   private
   
   def user_params
     params.require(:user).permit(:name, :email, :password,
-                                  :password_confirmation)
+                                  :password_confirmation, :region)
   end
+  
+  
 end
